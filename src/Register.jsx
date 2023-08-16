cd6e05cec2b32a624a3561d81842bb2830a5f5d3
Mudit Agrawal <muditagarwalgkp@gmail.com>
2023-08-16 22:17:02 +0530

    First

---
+++ src/Register.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+
+export const Register = (props) => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
+  return (
+    <div className="auth-form-container">
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Full name</label>
+        <input
+          value={name}
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          id="name"
+          placeholder="full Name"
+        />
+        <label htmlFor="email">email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          id="email"
+          name="email"
+        />
+        <label htmlFor="password">password</label>
+        <input
+          value={pass}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          id="password"
+          name="password"
+        />
+        <form className="row g-3">
+          
+          <div className="col-12">
+            <label htmlFor="inputAddress" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+            />
+          </div>
+        
+          <div className="col-md-6">
+            <label htmlFor="inputCity" className="form-label">
+              City
+            </label>
+            <input type="text" className="form-control" id="inputCity" />
+          </div>
+          <div className="col-md-4">
+            <label htmlFor="inputState" className="form-label">
+              State
+            </label>
+            <input type="text" className="form-control" id="inputState" />
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="inputZip" className="form-label">
+              Zip
+            </label>
+            <input type="text" className="form-control" id="inputZip" />
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                id="gridCheck"
+              />
+              <label className="form-check-label" htmlFor="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+        </form>
+        <button type="submit">Log In</button>
+      </form>
+      <button className="link-btn" onClick={() => props.onFormSwitch("login")}>
+        Already have an account? Login here.
+      </button>
+    </div>
+  );
+};
